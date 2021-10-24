8681b39b578c000874ac3c4541f79a24a91226e4
Vamsi75 <92527797+Vamsi75@users.noreply.github.com>
2021-10-24 09:32:18 +0530

    Update HandTracking.py

--- HandTracking.py
+++ HandTracking.py
@@ -1,15 +1,14 @@
 import cv2
 import mediapipe as mp
 import time
-
+#readind video input from opencv
 cap=cv2.VideoCapture(0)
 mpHands=mp.solutions.hands
 hands=mpHands.Hands()
 mpDraw=mp.solutions.drawing_utils
-
+#time for calculating framerate
 pTime=0
 cTime=0
-
 
 while True:
     success,img=cap.read()
@@ -19,7 +18,7 @@ while True:
     if res.multi_hand_landmarks:
         for handLms in res.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-                #print(id,lm)
+                #print(id,lm) -- to know the landmark id's
                 h, w, c=img.shape
                 cx,cy=int(lm.x*w),int(lm.y*h)
                 print(cx,cy)
@@ -30,7 +29,7 @@ while True:
     cTime=time.time()
     fps=1/(cTime-pTime)#to find framerate
     pTime=cTime
-
+    #displaying fps on screen
     cv2.putText(img,str(int(fps)),(10,70),cv2.FONT_HERSHEY_PLAIN,3,(255,0,100),3)
     cv2.imshow("image",img)
     cv2.waitKey(1)
